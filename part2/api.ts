84c44f626f47843c54960aa2542b07801b3291eb
MinhTue2k4 <artanis1607@gmail.com>
2026-08-21 22:28:34 +0700

    done p2

---
+++ part2/api.ts
@@ -0,0 +1,16 @@
+export interface HeroAPI {
+    localize_name: string;
+    img: string;
+}
+export interface Cards_of_heroes {
+    name: string;
+    image: string;
+}
+export async function getHeroAPI(): Promise<HeroAPI[]> {
+    const response = await fetch('https://api.opendota.com/api/heroStats');
+    if (!response.ok) {
+        throw new Error('Failed to fetch hero data')
+    }
+    const data: HeroAPI[] = await response.json();
+    return data;
+}
---
+++ part2/game.ts
@@ -0,0 +1,25 @@
+import type { HeroAPI, Cards_of_heroes } from "./api.js";
+export interface GameConfig {
+    Heroes_Data: HeroAPI[];
+    Cards_Data: Cards_of_heroes[];
+    deck_of_heroes(): Cards_of_heroes[];
+}
+export const game: GameConfig = {
+    Heroes_Data: [],
+    Cards_Data: [],
+
+    deck_of_heroes(): Cards_of_heroes[] {
+        //create a copy and take random from database  
+        const shuffled_all_heroes = [...this.Heroes_Data].sort(() => 0.5 - Math.random());
+    };
+    //take random 10 
+    const randomize_heroes = shuffled_all_heroes.slice(0, 10);
+    //saved heroes into a new array 
+    game.Cards_Data = randomize_heroes.map((hero) => {
+        return {
+                name: hero.localized_name,
+                image: 'https://cdn.steamstatic.com' + hero.img,
+            };
+    });
+// double the heroes 
+}
